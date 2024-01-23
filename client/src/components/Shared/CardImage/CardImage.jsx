eab520fc000cd316d0af6c927a7d8bde5509ad90
Theo Kimbangui <126973795+mb1mba@users.noreply.github.com>
2024-01-23 17:29:13 +0100

    Update CardImage.jsx

--- client/src/components/Shared/CardImage/CardImage.jsx
+++ client/src/components/Shared/CardImage/CardImage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const CardImage = ({ src }) => {
-  return <img className="rounded-xl h-full w-full" src={src} alt="" />;
+  return (
+    <img className="rounded-xl h-full w-full object-cover" src={src} alt="" />
+  );
 };
 
 export default CardImage;
