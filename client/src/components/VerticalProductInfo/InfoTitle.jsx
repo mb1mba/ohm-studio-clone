f3c1e8ea93c37472460f6f6d742791175529b359
Theo <126973795+mb1mba@users.noreply.github.com>
2024-02-03 00:01:54 +0100

    Update InfoTitle.jsx

--- client/src/components/VerticalProductInfo/InfoTitle.jsx
+++ client/src/components/VerticalProductInfo/InfoTitle.jsx
@@ -1,6 +1,6 @@
 const InfoTitle = ({ title }) => {
   return (
-    <h3 className="relative  font-helvetica cursor-pointer md:text-xl text-2xl after:content-[' '] after:translate-x-0 after:h-[1px] after:w-full after:bg-black after:absolute after:bottom-0 origin-left after:left-0 after:origin-left after:scale-x-0 hover:after:scale-x-100 after:duration-300   transition-transform">
+    <h3 className="relative font-helvetica cursor-pointer md:text-xl text-2xl after:content-[' '] after:translate-x-0 after:h-[1px] after:w-full after:bg-black after:absolute after:bottom-0  after:left-0 after:origin-right after:scale-x-0 hover:after:scale-x-100 after:duration-300 hover:after:origin-left after:transition-transform">
       {title.charAt(0).toUpperCase() + title.slice(1)}
     </h3>
   );
