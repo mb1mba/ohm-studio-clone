2a6bf30700c50eaf8c97041079cac428140b9f2e
Theo <126973795+mb1mba@users.noreply.github.com>
2024-02-05 23:46:21 +0100

    Create links folder
    Code more clean

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,2 +1,3 @@
 export { default as Button } from "./Button";
 export { default as CustomMenu } from "./CustomMenu";
+export { default as links } from "../Header/links";
---
+++ client/src/components/Header/links.js
@@ -0,0 +1,32 @@
+const links = {
+  Shop: [
+    {
+      name: "Pion",
+      path: "/collections/pion",
+    },
+    {
+      name: "Bloc",
+      path: "/collections/bloc",
+    },
+    {
+      name: "Ban",
+      path: "/collections/ban",
+    },
+  ],
+  About: [
+    {
+      name: "Who we are",
+      path: "#",
+    },
+    {
+      name: "Press",
+      path: "#",
+    },
+    {
+      name: "Dealers",
+      path: "#",
+    },
+  ],
+};
+
+export default links;
