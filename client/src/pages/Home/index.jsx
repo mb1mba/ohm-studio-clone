2007cc85b372078e98b9797d3b7033ed2f24a09f
Theo <126973795+mb1mba@users.noreply.github.com>
2024-02-12 22:33:07 +0100

    Improve home page for desktop

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import { Link } from "react-router-dom";
+import { motion, useInView, AnimatePresence } from "framer-motion";
 import {
   Text,
   Card,
@@ -10,9 +11,31 @@ import {
 import { transition } from "/src/components/Transition";
 
 const Home = () => {
+  const images = ["red.webp", "blue.webp", "green.webp", "yellow.webp"];
+
+  const randomIndex = Math.floor(Math.random() * 4);
+
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const imagesEl = images.map((image, i) => (
+    <motion.img
+      key={i}
+      loading="lazy"
+      width="860"
+      height="960"
+      src={`http://localhost:5500/uploads/${image}`}
+      style={{
+        display: i !== randomIndex ? "none" : "block",
+        position: "absolute",
+        bottom: 0,
+      }}
+    />
+  ));
+
   return (
     <>
-      <section className="grid w-full justify-center">
+      <section className="grid w-full justify-center h-screen md:min-w-screen md:justify-stretch md:h-[200vw] md:bg-yellow-100 md:flex md:z-[1] md:fixed md:top-0 ">
         <picture className="md:hidden">
           <img
             className=" rounded-t-xl w-full"
@@ -20,9 +43,8 @@ const Home = () => {
           />
         </picture>
 
-        <picture></picture>
-        <div className="w-full bg-[#f2eef0]">
-          <div className=" max-w-[86.67%] m-auto grid row-span-2 gap-10 pb-16">
+        <div className="w-full ">
+          <div className=" max-w-[86.67%] m-auto grid gap-10 pb-16 md:pt-40">
             <div className=" text-4xl">
               <div className="flex items-baseline w-full  ">
                 <Text>PION</Text>
@@ -66,203 +88,217 @@ const Home = () => {
             </Link>
           </div>
         </div>
+        <div className=" basis-full">{imagesEl}</div>
       </section>
 
-      <section>
-        <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
-          <div className="grid row-span-2 gap-4">
-            <p className=" font-helvetica text-xl">
-              The BLOC chair.
-              <br />1 metal sheet, 2 pleats, 4 screws.
-              <br /> Her line like her conception is clear simplicity,
-              triviality.
-            </p>
-            <Link
-              className=" font-helvetica text-[#8e9194]"
-              to="collections/bloc"
-            >
-              Discover
-            </Link>
-          </div>
+      <AnimatePresence>
+        <motion.div
+          className="bg-red-200 rounded-t-3xl m-auto md:z-[2]  md:relative md:rounded-xl  md:mt-[200vh] "
+          ref={ref}
+          initial={{ scale: 0.95 }}
+          animate={{
+            scale: isInView ? 1 : 0.95,
+            transition: { duration: 0.2, ease: "easeIn" },
+          }}
+          exit={{ scale: 0.95 }}
+        >
+          <section className="block ">
+            <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
+              <div className="grid row-span-2 gap-4">
+                <p className=" font-helvetica text-xl">
+                  The BLOC chair.
+                  <br />1 metal sheet, 2 pleats, 4 screws.
+                  <br /> Her line like her conception is clear simplicity,
+                  triviality.
+                </p>
+                <Link
+                  className=" font-helvetica text-[#8e9194]"
+                  to="collections/bloc"
+                >
+                  Discover
+                </Link>
+              </div>
 
-          <div className=" inline">
-            <span className=" font-garamond text-4xl">
-              — To make something better with less&nbsp;
-            </span>
-            <span className=" font-helvetica text-3xl">
-              To make something more beautiful with nothing. A story of blissful
-              sobriety. <br />
-              Like a child’s answer to an adult’s query.
-            </span>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
-          <CardDiv text="BLOC - Raw " price="1.125,00">
-            <Card size="md">
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-          </CardDiv>
-
-          <CardDiv text="BAN collection">
-            <Card size="md">
-              <CardImage src="images/home-img2.webp" />
-            </Card>
-          </CardDiv>
-        </div>
-      </section>
-
-      <section>
-        <div className="py-9 max-w-[86.67%] m-auto grid row-auto gap-10">
-          <div className="inline">
-            <span className="font-garamond text-4xl">
-              — A traditional approach&nbsp;
-            </span>
-            <span className=" font-helvetica text-3xl">
-              while imagining what the furniture of tomorrow could be, in its
-              use and representation.
-            </span>
-          </div>
-
-          <div className="grid row-span-2 gap-4">
-            <p className="font-helvetica text-xl">
-              OHM is a collectible furniture studio backed by a manufacturing
-              workshop.
-            </p>
-            <Link
-              className=" font-helvetica text-[#8e9194]"
-              to="collections/bloc"
-            >
-              Learn More
-            </Link>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
-          <CardDiv text="Orange PION created for COLLECTIBLE 2023">
-            <Card size="md">
-              <CardImage src="images/pion.webp" />
-            </Card>
-          </CardDiv>
-          <CardDiv text="PION - Orange" price="600">
-            <Card>
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-          </CardDiv>
-        </div>
-      </section>
-
-      <section>
-        <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
-          <div className="grid row-span-2 gap-4">
-            <p className="font-helvetica text-xl">
-              OHM is a collectible furniture studio backed by a manufacturing
-              workshop.
-            </p>
-            <Link
-              className=" font-helvetica text-[#8e9194]"
-              to="collections/bloc"
-            >
-              Learn More
-            </Link>
-          </div>
-          <div className=" inline">
-            <span className=" font-garamond text-4xl">
-              — Influenced by images know&nbsp;
-            </span>
-            <span className=" font-helvetica text-3xl">
-              to the human eye, our pieces of furniture are simple and yet
-              thought-through to be used in different ways.
-            </span>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
-          <CardDiv text="Orange PION created for COLLECTIBLE 2023">
-            <Card size="md">
-              <CardImage src="images/pion.webp" />
-            </Card>
-          </CardDiv>
-          <CardDiv text="PION - Orange" price="600">
-            <Card>
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-          </CardDiv>
-        </div>
-      </section>
-
-      <section>
-        <div className="max-w-[86.67%] m-auto grid row-span-3 gap-y-9">
-          <div>
-            <p className=" text-4xl font-helvetica font-medium">
-              Selected Products
-            </p>
-          </div>
-
-          <div className="grid grid-cols-2 grid-flow-row  gap-x-5 gap-y-9">
-            <Card size="sm">
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-            <Card size="sm">
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-            <Card size="sm">
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-            <Card size="sm">
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-            <Card size="sm">
-              <CardImage src="images/ban-gray.webp" />
-            </Card>
-          </div>
-
-          <div className="w-full text-center">
-            <Link className="" to="collections/pion">
-              See All
-            </Link>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="max-w-[86.67%] m-auto grid row-span-3 gap-y-9">
-          <CardDiv gap="20">
-            <div className="grid row-span-2 gap-6">
-              <Card size="lg">
-                <CardImage src="images/blue.webp" />
-              </Card>
-              <Card size="lg">
-                <CardText
-                  title="BLOC"
-                  path="/"
-                  text="Buy Now"
-                  description="Three aluminum chairs made in France in three versions : raw or brushed aluminum, and powder-coated blue."
-                />
-              </Card>
+              <div className=" inline">
+                <span className=" font-garamond text-4xl">
+                  — To make something better with less&nbsp;
+                </span>
+                <span className=" font-helvetica text-3xl">
+                  To make something more beautiful with nothing. A story of
+                  blissful sobriety. <br />
+                  Like a child’s answer to an adult’s query.
+                </span>
+              </div>
             </div>
-            <div className="grid row-span-2 gap-6  ">
-              <Card size="lg">
-                <CardImage src="images/blue.webp" />
-              </Card>
-              <Card size="lg">
-                <CardText
-                  title="PION"
-                  path="/"
-                  text="Buy Now"
-                  description="Four colored stools made in France from recycled steel, assembled by hand and then powder coated."
-                />
-              </Card>
+          </section>
+
+          <section>
+            <div className="py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
+              <CardDiv text="BLOC - Raw " price="1.125,00">
+                <Card size="md">
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+              </CardDiv>
+
+              <CardDiv text="BAN collection">
+                <Card size="md">
+                  <CardImage src="images/home-img2.webp" />
+                </Card>
+              </CardDiv>
             </div>
-          </CardDiv>
-        </div>
-      </section>
+          </section>
+
+          <section>
+            <div className="py-9 max-w-[86.67%] m-auto grid row-auto gap-10">
+              <div className="inline">
+                <span className="font-garamond text-4xl">
+                  — A traditional approach&nbsp;
+                </span>
+                <span className=" font-helvetica text-3xl">
+                  while imagining what the furniture of tomorrow could be, in
+                  its use and representation.
+                </span>
+              </div>
+
+              <div className="grid row-span-2 gap-4">
+                <p className="font-helvetica text-xl">
+                  OHM is a collectible furniture studio backed by a
+                  manufacturing workshop.
+                </p>
+                <Link
+                  className=" font-helvetica text-[#8e9194]"
+                  to="collections/bloc"
+                >
+                  Learn More
+                </Link>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
+              <CardDiv text="Orange PION created for COLLECTIBLE 2023">
+                <Card size="md">
+                  <CardImage src="images/pion.webp" />
+                </Card>
+              </CardDiv>
+              <CardDiv text="PION - Orange" price="600">
+                <Card>
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+              </CardDiv>
+            </div>
+          </section>
+
+          <section>
+            <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
+              <div className="grid row-span-2 gap-4">
+                <p className="font-helvetica text-xl">
+                  OHM is a collectible furniture studio backed by a
+                  manufacturing workshop.
+                </p>
+                <Link
+                  className=" font-helvetica text-[#8e9194]"
+                  to="collections/bloc"
+                >
+                  Learn More
+                </Link>
+              </div>
+              <div className=" inline">
+                <span className=" font-garamond text-4xl">
+                  — Influenced by images know&nbsp;
+                </span>
+                <span className=" font-helvetica text-3xl">
+                  to the human eye, our pieces of furniture are simple and yet
+                  thought-through to be used in different ways.
+                </span>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="  py-9 max-w-[86.67%] m-auto grid row-span-2 gap-10">
+              <CardDiv text="Orange PION created for COLLECTIBLE 2023">
+                <Card size="md">
+                  <CardImage src="images/pion.webp" />
+                </Card>
+              </CardDiv>
+              <CardDiv text="PION - Orange" price="600">
+                <Card>
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+              </CardDiv>
+            </div>
+          </section>
+
+          <section>
+            <div className="max-w-[86.67%] m-auto grid row-span-3 gap-y-9">
+              <div>
+                <p className=" text-4xl font-helvetica font-medium">
+                  Selected Products
+                </p>
+              </div>
+
+              <div className="grid grid-cols-2 grid-flow-row  gap-x-5 gap-y-9">
+                <Card size="sm">
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+                <Card size="sm">
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+                <Card size="sm">
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+                <Card size="sm">
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+                <Card size="sm">
+                  <CardImage src="images/ban-gray.webp" />
+                </Card>
+              </div>
+
+              <div className="w-full text-center">
+                <Link className="" to="collections/pion">
+                  See All
+                </Link>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="max-w-[86.67%] m-auto grid row-span-3 gap-y-9">
+              <CardDiv gap="20">
+                <div className="grid row-span-2 gap-6">
+                  <Card size="lg">
+                    <CardImage src="images/blue.webp" />
+                  </Card>
+                  <Card size="lg">
+                    <CardText
+                      title="BLOC"
+                      path="/"
+                      text="Buy Now"
+                      description="Three aluminum chairs made in France in three versions : raw or brushed aluminum, and powder-coated blue."
+                    />
+                  </Card>
+                </div>
+                <div className="grid row-span-2 gap-6  ">
+                  <Card size="lg">
+                    <CardImage src="images/blue.webp" />
+                  </Card>
+                  <Card size="lg">
+                    <CardText
+                      title="PION"
+                      path="/"
+                      text="Buy Now"
+                      description="Four colored stools made in France from recycled steel, assembled by hand and then powder coated."
+                    />
+                  </Card>
+                </div>
+              </CardDiv>
+            </div>
+          </section>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
