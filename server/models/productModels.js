3d6471c6c98c16539ea278798d14746ab1228fd4
Theo <126973795+mb1mba@users.noreply.github.com>
2024-02-21 23:18:15 +0100

    Update productModels.js

--- server/models/productModels.js
+++ server/models/productModels.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   images: { type: Array },
-  description: { type: String, required: true },
+  description: { type: Array, required: true },
   deliveryTime: { type: String, required: true },
   variants: [
     {
