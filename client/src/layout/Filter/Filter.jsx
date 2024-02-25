5fb915ae98ad9d6b72a278332094ef7f821a02c3
Theo <126973795+mb1mba@users.noreply.github.com>
2024-02-26 00:00:02 +0100

    Update Filter.jsx

--- client/src/layout/Filter/Filter.jsx
+++ client/src/layout/Filter/Filter.jsx
@@ -20,19 +20,23 @@ const links = [
 const Filter = () => {
   const { collectionName } = useParams();
 
+  const styles = {
+    selected:
+      "md:text-[1.3vw] cursor-pointer text-[#1119] text-[1vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-1 ",
+    notSelected:
+      " md:text-[1.3vw] cursor-pointer text-[#1119] text-[1vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-0 hover:before:scale-x-100  hover:before:origin-left before:delay-200 before:transition-transform before:duration-300  after:content-[' ']  after:h-[1px] after:w-full after:bg-[#8e9194] after:absolute after:bottom-0  after:left-0 before after:origin-right after:scale-x-0 hover:after:scale-x-[-100%] after:duration-700 hover:after:origin-right after:transition-transform",
+  };
+
   const element = links.map((link) => {
-    const style = `md:text-[1.3vw] cursor-pointer text-[#1119]
-    ${
-      collectionName === link.title.toLowerCase() &&
-      "underline underline-offset-[10px] text-[#000]"
-    }`;
+    const style =
+      collectionName === link.title.toLowerCase()
+        ? styles["selected"]
+        : styles["notSelected"];
 
     return (
       <li className="relative max-w-fit overflow-hidden cursor-pointer">
-        <NavLink className={style} to={link.path}>
-          <span className="text-[1vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-0 hover:before:scale-x-100  hover:before:origin-left before:delay-200 before:transition-transform before:duration-300  after:content-[' ']  after:h-[1px] after:w-full after:bg-[#8e9194] after:absolute after:bottom-0  after:left-0 before after:origin-right after:scale-x-0 hover:after:scale-x-[-100%] after:duration-700 hover:after:origin-right after:transition-transform">
-            {link.title}
-          </span>
+        <NavLink className="relative h-fit" to={link.path}>
+          <span className={style}>{link.title}</span>
         </NavLink>
       </li>
     );
@@ -42,7 +46,7 @@ const Filter = () => {
     <>
       <div className="pt-44 px-5 md:px-[2vw] pb-16 ">
         <div className="grid row-auto columns-auto">
-          <h1 className=" font-helvetica text-4xl font-bold pb-5  md:pb-[1vw] md:text-[2vw]">
+          <h1 className=" font-ehlvetica text-4xl font-bold pb-5  md:pb-[1vw] md:text-[2vw]">
             Products
           </h1>
           <Drawline />
