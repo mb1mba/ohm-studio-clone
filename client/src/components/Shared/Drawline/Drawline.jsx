c980d0d026574b6a8383d224b3ec9c2436f4115a
Theo <126973795+mb1mba@users.noreply.github.com>
2024-02-28 17:02:39 +0100

    Add exit and add className prop

--- client/src/components/Shared/Drawline/Drawline.jsx
+++ client/src/components/Shared/Drawline/Drawline.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
-const Drawline = ({ isAnimate = true }) => {
+const Drawline = ({
+  isAnimate = true,
+  className = "bg-[#8e91944d] h-[1px]",
+}) => {
   return (
     <motion.div
       variants={{
         hidden: { width: 0 },
         visible: { width: "100%" },
+        exit: { width: 0 },
       }}
       initial="hidden"
       animate="visible"
+      exit="exit"
       transition={isAnimate && { duration: 0.75, ease: "linear" }}
-      className=" bg-[#8e91944d] h-[1px]"
+      className={className}
     ></motion.div>
   );
 };
