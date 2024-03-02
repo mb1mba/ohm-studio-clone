ab064fbc37b7fd56360496ed66e1e4e25854e464
Theo <126973795+mb1mba@users.noreply.github.com>
2024-03-03 00:42:43 +0100

    Update Sentence.jsx
    Add block so all string will have their own line

--- client/src/components/Sentence/Sentence.jsx
+++ client/src/components/Sentence/Sentence.jsx
@@ -4,13 +4,13 @@ const Sentence = ({ array }) => {
   const sen = array.map((s, i) => {
     if (i === 0) {
       return (
-        <span className="font-garamond  leading-[0.8] md:leading-[1.25] text-[9.25vw] md:text-[3.35vw] 3xl:text-6xl  ">
-          {s}&nbsp;
+        <span className="font-garamond  block  leading-[0.8] md:leading-[1.25] text-[9.25vw] md:text-[3.35vw] 3xl:text-6xl  ">
+          —{s}&nbsp;
         </span>
       );
     } else {
       return (
-        <span className="font-helvetica leading-[1.2] md:leading-[1.25] font-semibold text-[8vw] md:text-[3vw] 3xl:text-[3rem]  ">
+        <span className="font-helvetica block leading-[1.2] md:leading-[1.25] font-semibold text-[8vw] md:text-[3vw] 3xl:text-[3rem]  ">
           {s}
         </span>
       );
