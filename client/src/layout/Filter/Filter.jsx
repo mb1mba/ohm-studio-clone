c90b7fccb71aa1ff8c4cd5ba21cf424f9d17908d
Theo <126973795+mb1mba@users.noreply.github.com>
2024-03-03 00:44:45 +0100

    Update Filter.jsx
    Improve css

--- client/src/layout/Filter/Filter.jsx
+++ client/src/layout/Filter/Filter.jsx
@@ -22,9 +22,9 @@ const Filter = () => {
 
   const styles = {
     selected:
-      "md:text-[1.3vw] cursor-pointer text-[#111111]  text-[1vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-1 ",
+      "md:text-[1.3vw] cursor-pointer text-[#111111]  text-[4.8vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-1 ",
     notSelected:
-      " md:text-[1.3vw] cursor-pointer text-[#1119] text-[1vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-0 hover:before:scale-x-100  hover:before:origin-left before:delay-200 before:transition-transform before:duration-300  after:content-[' ']  after:h-[1px] after:w-full after:bg-[#8e9194] after:absolute after:bottom-0  after:left-0 before after:origin-right after:scale-x-0 hover:after:scale-x-[-100%] after:duration-700 hover:after:origin-right after:transition-transform",
+      " md:text-[1.3vw] cursor-pointer text-[#1119] text-[4.8vw] 3xl:text-2xl before:h-[1px] before:w-full before:bg-[#8e9194] before:absolute before:bottom-0  before:left-0 before:origin-right before:scale-x-0 hover:before:scale-x-100  hover:before:origin-left before:delay-200 before:transition-transform before:duration-300  after:content-[' ']  after:h-[1px] after:w-full after:bg-[#8e9194] after:absolute after:bottom-0  after:left-0 before after:origin-right after:scale-x-0 hover:after:scale-x-[-100%] after:duration-700 hover:after:origin-right after:transition-transform",
   };
 
   const element = links.map((link) => {
